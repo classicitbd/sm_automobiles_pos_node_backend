4eaf319a58b0701bcea99d39928fc5acebe7df76
nazmulbhuyian <nazmulbhuyian000@gmail.com>
2024-12-24 15:53:58 +0600

    done

--- src/app/role/role.model.ts
+++ src/app/role/role.model.ts
@@ -21,83 +21,223 @@ const roleSchema = new Schema<IRoleInterface>(
       type: Boolean,
       default: false,
     },
-    category_show: {
+    category_post: {
       type: Boolean,
       default: false,
     },
-    category_create: {
+    category_patch: {
       type: Boolean,
       default: false,
     },
-    category_update: {
+    brand_post: {
       type: Boolean,
       default: false,
     },
-    category_delete: {
+    brand_patch: {
       type: Boolean,
       default: false,
     },
-    brand_show: {
+    unit_dashboard_show: {
       type: Boolean,
       default: false,
     },
-    brand_create: {
+    unit_post: {
       type: Boolean,
       default: false,
     },
-    brand_update: {
+    unit_patch: {
       type: Boolean,
       default: false,
     },
-    brand_delete: {
+    site_setting_patch: {
       type: Boolean,
       default: false,
     },
-    product_show: {
+    user_post: {
       type: Boolean,
       default: false,
     },
-    product_create: {
+    user_patch: {
       type: Boolean,
       default: false,
     },
-    product_update: {
+    user_dashboard_show: {
       type: Boolean,
       default: false,
     },
-    product_delete: {
+    role_post: {
       type: Boolean,
       default: false,
     },
-    staff_show: {
+    role_patch: {
       type: Boolean,
       default: false,
     },
-    staff_create: {
+    sale_target_post: {
       type: Boolean,
       default: false,
     },
-    staff_update: {
+    sale_target_patch: {
       type: Boolean,
       default: false,
     },
-    staff_delete: {
+    supplier_dashboard_show: {
       type: Boolean,
       default: false,
     },
-    staff_permission_show: {
+    supplier_post: {
       type: Boolean,
       default: false,
     },
-    staff_permission_create: {
+    supplier_patch: {
       type: Boolean,
       default: false,
     },
-    staff_permission_update: {
+    supplier_payment_post: {
       type: Boolean,
       default: false,
     },
-    staff_permission_delete: {
+    supplier_payment_patch: {
+      type: Boolean,
+      default: false,
+    },
+    supplier_paid_payment_show: {
+      type: Boolean,
+      default: false,
+    },
+    supplier_unpaid_payment_show: {
+      type: Boolean,
+      default: false,
+    },
+    supplier_check_or_cash_out_payment_show: {
+      type: Boolean,
+      default: false,
+    },
+    supplier_payment_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    cash_patch: {
+      type: Boolean,
+      default: false,
+    },
+    bank_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    bank_post: {
+      type: Boolean,
+      default: false,
+    },
+    bank_patch: {
+      type: Boolean,
+      default: false,
+    },
+    customer_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    customer_post: {
+      type: Boolean,
+      default: false,
+    },
+    customer_patch: {
+      type: Boolean,
+      default: false,
+    },
+    check_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    customer_ar_show: {
+      type: Boolean,
+      default: false,
+    },
+    check_today_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    check_due_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    check_or_cash_in_payment_show: {
+      type: Boolean,
+      default: false,
+    },
+    product_post: {
+      type: Boolean,
+      default: false,
+    },
+    product_patch: {
+      type: Boolean,
+      default: false,
+    },
+    product_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    stock_post: {
+      type: Boolean,
+      default: false,
+    },
+    stock_ap_show: {
+      type: Boolean,
+      default: false,
+    },
+    expense_show: {
+      type: Boolean,
+      default: false,
+    },
+    expense_post: {
+      type: Boolean,
+      default: false,
+    },
+    income_show: {
+      type: Boolean,
+      default: false,
+    },
+    order_post: {
+      type: Boolean,
+      default: false,
+    },
+    order_patch: {
+      type: Boolean,
+      default: false,
+    },
+    order_dashboard_show: {
+      type: Boolean,
+      default: false,
+    },
+    profit_show: {
+      type: Boolean,
+      default: false,
+    },
+    management_order_show: {
+      type: Boolean,
+      default: false,
+    },
+    account_order_show: {
+      type: Boolean,
+      default: false,
+    },
+    warehouse_order_show: {
+      type: Boolean,
+      default: false,
+    },
+    out_of_warehouse_order_show: {
+      type: Boolean,
+      default: false,
+    },
+    ledger_show: {
+      type: Boolean,
+      default: false,
+    },
+    order_invoice_print: {
+      type: Boolean,
+      default: false,
+    },
+    order_challan_print: {
       type: Boolean,
       default: false,
     },
